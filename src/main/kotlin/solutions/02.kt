9e5e3eb575751e8479efd5c5c54573fb8bc6fa8e
stianste <stiansteinbakken94@gmail.com>
2020-12-02 19:36:23 +0100

    Add solution to second part of day two

--- src/main/kotlin/solutions/02.kt
+++ src/main/kotlin/solutions/02.kt
@@ -7,10 +7,19 @@ fun main() {
 
     fun getNumberOfOccurencesInString(s: String, char: Char) = s.filter { it == char }.count()
 
+
     fun lineIsValid(line: String): Boolean {
         val (range, letter, password) = line.split(" ")
         val (minValue, maxValue) = range.split("-").map { it.toInt() }
         return getNumberOfOccurencesInString(password, letter.toCharArray().first()) in (minValue..maxValue)
+    }
+
+    fun lineIsValidAccordingToSecondPolicy(line: String): Boolean {
+        val (range, letter, password) = line.split(" ")
+        val (firstIndex, secondIndex) = range.split("-").map { it.toInt() - 1 }
+        val char = letter.toCharArray().first()
+        return (password[firstIndex] == char && password[secondIndex] != char)
+                || (password[firstIndex] != char && password[secondIndex] == char)
     }
 
     fun partOne() {
@@ -18,11 +27,18 @@ fun main() {
         assert(lineIsValid("2-9 c ccccccccc"))
 
         assert(!lineIsValid("1-3 b cdefg"))
-        println(lines.filter { lineIsValid(it) }.count())
+        println("Solution part 1: ${lines.filter { lineIsValid(it) }.count()}")
     }
 
     fun partTwo() {
+        assert(lineIsValidAccordingToSecondPolicy("1-3 a abcde"))
+
+        assert(!lineIsValidAccordingToSecondPolicy("2-9 c ccccccccc"))
+        assert(!lineIsValidAccordingToSecondPolicy("1-3 b cdefg"))
+
+        println("Solution part 2: ${lines.filter { lineIsValidAccordingToSecondPolicy(it) }.count()}")
     }
 
     partOne()
+    partTwo()
 }

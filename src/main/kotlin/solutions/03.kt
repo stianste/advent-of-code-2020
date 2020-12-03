2767c5560c685c0260087ca41e65bdd2e169863d
stianste <stiansteinbakken94@gmail.com>
2020-12-03 11:14:09 +0100

    Add solution to part two of day 3

--- src/main/kotlin/solutions/03.kt
+++ src/main/kotlin/solutions/03.kt
@@ -4,17 +4,49 @@ import main.kotlin.readLinesFromFile
 
 fun main() {
     val lines = readLinesFromFile("./src/resources/03.txt")
-    val width = lines.first().length
-    val widthIncrement = 3
+
+    fun generalSolution(right: Int, down: Int, lines: List<String>) =
+        lines.windowed(size = 1, step = down).mapIndexed { x, line ->
+            line.first()[x * right % line.first().length]
+        }.count { it == '#' }
 
     fun partOne() {
-        val result = lines.mapIndexed{ index, line ->
-            line[index * widthIncrement % width]
-        }.count { it == '#' }
+        val result = generalSolution(3, 1, lines)
+        assert(result == 262)
         println("Number of trees part 1: $result")
     }
 
     fun partTwo() {
+        val exampleLines = listOf(
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#"
+        )
+
+        val targets = listOf(
+            Pair(1, 1),
+            Pair(3, 1),
+            Pair(5, 1),
+            Pair(7, 1),
+            Pair(1, 2)
+        )
+
+        val exampleSolutions = targets.map { generalSolution(it.first, it.second, exampleLines) }
+        val exampleProduct = exampleSolutions.reduce { a, b -> a * b }
+        assert(exampleProduct == 336)
+        println("Product of example: $exampleProduct. Partial solutions: $exampleSolutions")
+
+        val solutions = targets.map { generalSolution(it.first, it.second, lines) }
+        val solutionsProduct = solutions.map { it.toBigDecimal() }.reduce { a, b -> a * b }
+        println("Product of trees part 2: $solutionsProduct. Partial solutions: $solutions")
     }
 
     partOne()
